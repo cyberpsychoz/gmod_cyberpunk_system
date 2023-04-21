9bab47dba1bb5ae64cfbd04934dcb0ec22b9b2f2
Крыжовник <vasyl.lebedev@yandex.ru>
2023-04-22 01:57:11 +0300

    Update sh_plugin.lua

--- sh_plugin.lua
+++ sh_plugin.lua
@@ -8,13 +8,27 @@ function GM:PlayerButtonDown(ply, button)
         if ply:GetNWBool("CombatMode") then
             ply:SetMoveType(MOVETYPE_NONE)
             timer.Simple(0.1, function()
-                ply:ChatPrint(ply:Nick() .. " включил боевой режим!")
+                ply:ChatPrint(ply:Nick() .. " вошел в боевой режим!")
             end)
         else
             ply:SetMoveType(MOVETYPE_WALK)
             timer.Simple(0.1, function()
-                ply:ChatPrint(ply:Nick() .. " выключил боевой режим!")
+                ply:ChatPrint(ply:Nick() .. " вышел из боевого режима!")
             end)
         end
     end
 end
+
+hook.Add("OnContextMenuOpen", "AddTargetOption", function()
+    local menu = DermaMenu()
+    for _, ply in ipairs(player.GetAll()) do
+        if ply ~= LocalPlayer() then
+            menu:AddOption("Сделать целью", function()
+                net.Start("SetTarget")
+                net.WriteEntity(ply)
+                net.SendToServer()
+            end)
+        end
+    end
+    menu:Open()
+end)
