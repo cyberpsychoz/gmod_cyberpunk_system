5f837a785a79ce62614a2c318261ab7d369df70c
Крыжовник <vasyl.lebedev@yandex.ru>
2023-04-22 01:44:26 +0300

    Update sh_plugin.lua

--- sh_plugin.lua
+++ sh_plugin.lua
@@ -2,15 +2,19 @@ PLUGIN.name = "Cyberpunk system"
 PLUGIN.author = "Крыжовник#4511"
 PLUGIN.desc = "Плагин вдохновленный схемой ReHost'а."
 
-hook.Add("PlayerButtonDown", "CombatMode", function(ply, button)
-    if button == KEY_7 then
+function GM:PlayerButtonDown(ply, button)
+    if button == KEY_H then
         ply:SetNWBool("CombatMode", !ply:GetNWBool("CombatMode"))
         if ply:GetNWBool("CombatMode") then
             ply:SetMoveType(MOVETYPE_NONE)
-            ply:ChatPrint("Вы включили боевой режим!")
+            timer.Simple(0.1, function()
+                ply:ChatPrint(ply:Nick() .. " включил боевой режим!")
+            end)
         else
             ply:SetMoveType(MOVETYPE_WALK)
-            ply:ChatPrint("Вы выключили боевой режим!")
+            timer.Simple(0.1, function()
+                ply:ChatPrint(ply:Nick() .. " выключил боевой режим!")
+            end)
         end
     end
-end)
+end
